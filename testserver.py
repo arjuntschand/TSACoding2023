b6e6914259a9194f57aba6dce483d1497ed8414e
arjuntschand <arjun@voicesaver.com>
2023-03-25 19:17:29 -0400

    init

--- testserver.py
+++ testserver.py
@@ -1,25 +1,15 @@
-from flask import Flask, request, jsonify # Imports the flask library modules
-import pandas as pd
-import numpy as np # linear algebra
-from datetime import datetime, timedelta
-import requests
-import json
-import datetime
-import math
+from flask import Flask
 
 app = Flask(__name__)
-print("hi")
-@app.route('/')
+@app.route('/match')
+def match():
+    return "hi"
 
-
-
-def home():
+app = Flask(__name__)
+@app.route('/foster')
+def match():
     return "hi"
 
 if __name__ == '__main__':
     # run app in debug mode on port 5000
-    #app.run(debug=True, host="192.168.87.250", port=5000)
-    app.run(debug=True, host="192.168.86.27", port=5000)
-
-print("hi")
-hello
+    app.run(debug=False, host="192.168.86.27", port=5000)
